988d69a8aae532f97811f4b1dea6c79ec03ad8d4
kajal mia <kundefine@gmail.com>
2026-09-17 19:23:19 +0600

    index on product and categories

--- database/migrations/2026_09_16_172711_create_products_table.php
+++ database/migrations/2026_09_16_172711_create_products_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('products', function (Blueprint $table) {
             $table->id();
             $table->foreignId('category_id')->nullable()->constrained()->cascadeOnDelete();
-            $table->string('name');
+            $table->string('name')->index();
             $table->string('slug');
             $table->text('description')->nullable();
             $table->decimal('price', 8, 2);
